2125b62763bd28662670244de40d477cd4e387d8
ToronBurger <coko887702@gmail.com>
2023-09-04 19:31:36 +0800

    add new file

---
+++ new_file.py
@@ -0,0 +1,14 @@
+#整數 interger int
+#浮點數(小數) float
+#布林值 boolean bool (True , False)
+#字串 string str (文字) '123' '文字' ''
+price = 10
+pen = 0.38
+rain = True
+name = 'allen'
+print(price)
+print(pen)
+print(rain)
+print(name)
+print(price , pen , rain , name)
+
